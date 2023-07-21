31c5098aed3f67cb3689a6f1906a45f18af063c1
Richard Lindner <rlindner81@gmail.com>
2023-07-21 15:13:16 +0200

    fix tests

--- test/__mocks/shared/static.js
+++ test/__mocks/shared/static.js
@@ -2,8 +2,11 @@
 
 const sharedStatic = jest.requireActual("../../../src/shared/static");
 
-module.exports = Object.fromEntries(
-  Object.getOwnPropertyNames(sharedStatic).map((key) => {
-    return [key, jest.fn(sharedStatic[key].bind(sharedStatic))];
-  })
-);
+module.exports = Object.entries(sharedStatic).reduce((acc, [key, value]) => {
+  if (typeof value === "function") {
+    acc[key] = jest.fn(value.bind(sharedStatic));
+  } else {
+    acc[key] = value;
+  }
+  return acc;
+}, {});
