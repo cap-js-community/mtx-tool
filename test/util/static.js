4d673551ce7397f9ea818ade107ae15a778a4e09
Richard Lindner <rlindner81@gmail.com>
2023-10-05 13:55:09 +0200

    anonymize response times in test logs

--- test/util/static.js
+++ test/util/static.js
@@ -9,7 +9,8 @@ const outputFromLoggerPartitionFetch = (calls) => {
   const [fetchLogLines, logLines] = partition(inputLogLines, (line) =>
     /^(?:GET|POST|PATCH|DELETE) https:\/\//i.test(line)
   );
-  return [...logLines, "", ...fetchLogLines].join("\n");
+  const fetchLogLinesWithMockTimestamp = fetchLogLines.map((line) => line.replace(/\(\d+ms\)/g, "(88ms)"));
+  return [...logLines, "", ...fetchLogLinesWithMockTimestamp].join("\n");
 };
 
 module.exports = {
