cc21c9398d5cde2b88caf12eba8e3014c787978b
Richard Lindner <rlindner81@gmail.com>
2024-12-05 15:26:36 +0100

    add tests for hdi tunnel

--- test/hanaManagement.test.js
+++ test/hanaManagement.test.js
@@ -5,11 +5,13 @@ jest.mock("../src/shared/request", () => ({
   request: jest.fn(),
 }));
 
+const mockStatic = require("../src/shared/static");
 jest.mock("../src/shared/static", () => {
   const staticLib = jest.requireActual("../src/shared/static");
   return {
     ...staticLib,
     sleep: jest.fn(),
+    isPortFree: jest.fn(),
   };
 });
 
@@ -20,6 +22,8 @@ jest.mock("../src/shared/logger", () => require("./__mocks/shared/logger"));
 const hdi = require("../src/submodules/hanaManagement");
 const { outputFromLogger } = require("./util/static");
 
+const testTenantId = "5ecc7413-2b7e-414a-9496-ad4a61f6cccf";
+
 const mockCredentials = {
   certurl: "uaa-cert-url",
   clientid: "clientid",
@@ -29,9 +33,11 @@ const mockCredentials = {
   xsappname: "xsappname",
 };
 
+const mockCfSsh = jest.fn();
+
 const mockContext = {
   getHdiInfo() {
-    return { cfService: { credentials: mockCredentials } };
+    return { cfSsh: mockCfSsh, cfService: { credentials: mockCredentials } };
   },
   getCachedUaaToken() {
     return "token";
@@ -64,6 +70,10 @@ const mockBindingFactory = (i) => ({
 });
 
 describe("hdi tests", () => {
+  afterEach(() => {
+    hdi._._reset();
+  });
+
   test("enable native tenant already enabled", async () => {
     const n = 4;
 
@@ -116,6 +126,61 @@ describe("hdi tests", () => {
       created 1 missing binding for tenant tenant-id-1
       created 1 missing binding for tenant tenant-id-2
       created 1 missing binding for tenant tenant-id-3"
+    `);
+    expect(mockLogger.error).toHaveBeenCalledTimes(0);
+  });
+
+  test("hdi tunnel", async () => {
+    mockRequest.request.mockReturnValueOnce({
+      async json() {
+        return { items: [{ id: "service-plan-id" }] };
+      },
+    });
+    mockRequest.request.mockReturnValueOnce({
+      async json() {
+        return {
+          items: [
+            {
+              credentials: {
+                host: "host",
+                port: "port",
+                url: "jdbc:sap://host:port",
+                user: "user",
+                password: "password",
+                schema: "schema",
+                hdi_user: "hdi-user",
+                hdi_password: "hdi-password",
+              },
+              labels: {
+                tenant_id: [testTenantId],
+              },
+            },
+          ],
+        };
+      },
+    });
+    mockStatic.isPortFree.mockReturnValueOnce(true);
+    mockCfSsh.mockReturnValueOnce();
+
+    await expect(hdi.hdiTunnelTenant(mockContext, [testTenantId], [false])).resolves.toBeUndefined();
+
+    expect(mockRequest.request).toHaveBeenCalledTimes(2);
+    expect(mockStatic.isPortFree).toHaveBeenCalledTimes(1);
+    expect(mockCfSsh).toHaveBeenCalledTimes(1);
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
+      "
+      runtime
+      localUrl   jdbc:sap://localhost:30015
+      remoteUrl  jdbc:sap://host:port      
+      user       user                      
+      password   *** show with --reveal ***
+
+      designtime
+      localUrl   jdbc:sap://localhost:30015
+      remoteUrl  jdbc:sap://host:port      
+      user       hdi-user                  
+      password   *** show with --reveal ***
+      "
     `);
     expect(mockLogger.error).toHaveBeenCalledTimes(0);
   });
