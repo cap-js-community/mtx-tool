ba0f1de01eb15ed302af98cdb82904f62a8d50fb
Richard Lindner <rlindner81@gmail.com>
2024-12-06 14:44:54 +0100

    start with request unit tests

--- test/shared/request.test.js
+++ test/shared/request.test.js
@@ -1,82 +1,76 @@
 "use strict";
+const { request } = require("../../src/shared/request");
 
-jest.mock("node-fetch", () => jest.fn());
-jest.mock("../../src/shared/static", () => ({
-  sleep: jest.fn(),
-}));
 const mockFetchLib = require("node-fetch");
-const mockShared = require("../../src/shared/static");
+jest.mock("node-fetch", () => jest.fn());
 
 const { Logger: MockLogger } = require("../../src/shared/logger");
+const { outputFromLogger } = require("../test-util/static");
 const mockLogger = MockLogger.getInstance();
 jest.mock("../../src/shared/logger", () => require("../__mocks/shared/logger"));
 
-const { request } = require("../../src/shared/request");
-
-const options = {
-  url: "https://fake.com",
+const baseOkResponse = {
+  ok: true,
+  status: 200,
+  statusText: "OK",
 };
 
-const normalizeMockCallTimings = (calls) => calls.map((call) => call.map((args) => args.replace(/\d+ms/g, "xms")));
+const baseBadRequestResponse = {
+  ok: false,
+  status: 400,
+  statusText: "Bad Request",
+  text() {
+    return "failure reason";
+  },
+};
 
-describe("request tests", () => {
-  test("handling too many requests with falloff", async () => {
-    const busyResponseFactory = (index) =>
-      Promise.resolve({
-        index,
-        status: 429,
-        statusText: "Too Many Requests",
-        text: () => Promise.resolve("too many requests, try again later"),
-      });
-    const responseCount = 5;
-    const fakeResponses = Array.from({ length: responseCount }).map((_, i) => busyResponseFactory(i));
-    for (const fakeResponse of fakeResponses) {
-      mockFetchLib.mockReturnValueOnce(fakeResponse);
-    }
+describe("request", () => {
+  test("basic ok", async () => {
+    mockFetchLib.mockReturnValueOnce(baseOkResponse);
+    await request({ url: "https://server", pathname: "/path" });
+    expect(mockFetchLib.mock.calls).toMatchSnapshot();
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
+      `"GET https://server/path 200 OK (0ms)"`
+    );
+  });
 
-    const responsePromise = request(options);
-
-    await expect(responsePromise).rejects.toMatchInlineSnapshot(`
-            [Error: got bad response 429 from https://fake.com/
-            too many requests, try again later]
+  test("basic bad request", async () => {
+    mockFetchLib.mockReturnValueOnce(baseBadRequestResponse);
+    await expect(request({ url: "https://server", pathname: "/path" })).rejects.toMatchInlineSnapshot(`
+            [Error: got bad response 400 from https://server/path
+            failure reason]
           `);
-    expect(mockShared.sleep.mock.calls).toMatchInlineSnapshot(`
-      [
-        [
-          6000,
-        ],
-        [
-          12000,
-        ],
-        [
-          24000,
-        ],
-        [
-          48000,
-        ],
-      ]
-    `);
+    expect(mockFetchLib.mock.calls).toMatchSnapshot();
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
+      `"GET https://server/path 400 Bad Request (0ms)"`
+    );
+  });
 
-    expect(mockFetchLib).toHaveBeenCalledTimes(responseCount);
-    expect(normalizeMockCallTimings(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
-      [
-        [
-          "GET https://fake.com/ 429 Too Many Requests (xms) retrying in 6sec",
-        ],
-        [
-          "GET https://fake.com/ 429 Too Many Requests (xms) retrying in 12sec",
-        ],
-        [
-          "GET https://fake.com/ 429 Too Many Requests (xms) retrying in 24sec",
-        ],
-        [
-          "GET https://fake.com/ 429 Too Many Requests (xms) retrying in 48sec",
-        ],
-        [
-          "GET https://fake.com/ 429 Too Many Requests (xms)",
-        ],
-      ]
-    `);
-    expect(mockLogger.error).toHaveBeenCalledTimes(0);
+  test("bad request unchecked", async () => {
+    mockFetchLib.mockReturnValueOnce(baseBadRequestResponse);
+    await request({ url: "https://server", pathname: "/path", checkStatus: false });
+    expect(mockFetchLib.mock.calls).toMatchSnapshot();
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
+      `"GET https://server/path 400 Bad Request (0ms)"`
+    );
+  });
+
+  test("ok with no logging", async () => {
+    mockFetchLib.mockReturnValueOnce(baseOkResponse);
+    await request({ url: "https://server", pathname: "/path", logged: false });
+    expect(mockFetchLib.mock.calls).toMatchSnapshot();
+    expect(mockLogger.info).toHaveBeenCalledTimes(0);
+  });
+
+  test("ok with basic auth", async () => {
+    mockFetchLib.mockReturnValueOnce(baseOkResponse);
+    await request({ url: "https://server", pathname: "/path", auth: { username: "username", password: "password" } });
+    expect(mockFetchLib.mock.calls).toMatchSnapshot();
+  });
+
+  test("ok with bearer auth", async () => {
+    mockFetchLib.mockReturnValueOnce(baseOkResponse);
+    await request({ url: "https://server", pathname: "/path", auth: { token: "token" } });
+    expect(mockFetchLib.mock.calls).toMatchSnapshot();
   });
 });
