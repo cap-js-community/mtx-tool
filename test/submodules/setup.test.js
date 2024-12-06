d302004b2e4ff15984b313b9fa04d5a85a9cf735
Richard Lindner <rlindner81@gmail.com>
2024-12-06 11:11:38 +0100

    problem test fails in a stable manner

--- test/submodules/setup.test.js
+++ test/submodules/setup.test.js
@@ -7,8 +7,12 @@ jest.mock("../../src/shared/logger", () => require("../__mocks/shared/logger"));
 const mockStatic = require("../../src/shared/static");
 jest.mock("../../src/shared/static", () => ({
   tryAccessSync: jest.fn(),
-  tryReadJsonSync: jest.fn(),
   question: jest.fn(),
+}));
+
+const mockContextModule = require("../../src/context");
+jest.mock("../../src/context", () => ({
+  readRuntimeConfig: jest.fn(),
 }));
 
 const mockFs = require("fs");
@@ -43,7 +47,7 @@ describe("set tests", () => {
 
   test("setup list", async () => {
     mockStatic.tryAccessSync.mockReturnValue(true);
-    mockStatic.tryReadJsonSync.mockReturnValueOnce(mockRuntimeConfig);
+    mockContextModule.readRuntimeConfig.mockReturnValueOnce(mockRuntimeConfig);
 
     expect(set.setupList()).toMatchInlineSnapshot(`
       "1/5 | cf app bound to xsuaa service (optional)? mock-uaa-app
@@ -52,24 +56,25 @@ describe("set tests", () => {
       4/5 | cf app bound to service-manager or managed-hana service (optional)? mock-hdi-app
       5/5 | cf app with "/info" endpoint (optional)? mock-srv-app"
     `);
-    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
-      `"using runtime config root/local-dir/.mtxrc.json"`
-    );
+    expect(mockStatic.tryAccessSync).toHaveBeenCalledTimes(1);
+    expect(mockContextModule.readRuntimeConfig).toHaveBeenCalledTimes(1);
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`""`);
     expect(mockLogger.error).toHaveBeenCalledTimes(0);
   });
 
-  test("setup local", async () => {
-    mockStatic.tryAccessSync.mockReturnValueOnce(true);
-    mockStatic.tryReadJsonSync.mockReturnValueOnce(mockRuntimeConfig);
+  test("setup global", async () => {
+    mockContextModule.readRuntimeConfig.mockReturnValueOnce(mockRuntimeConfig);
     for (let i = 0; i < Object.keys(mockRuntimeConfig).length; i++) {
       mockStatic.question.mockReturnValueOnce(`answer ${i + 1}`);
     }
 
-    expect(await set.setupLocal()).toMatchInlineSnapshot(`undefined`);
+    expect(await set.setup()).toMatchInlineSnapshot(`undefined`);
+    expect(mockStatic.tryAccessSync).toHaveBeenCalledTimes(0);
+    expect(mockContextModule.readRuntimeConfig).toHaveBeenCalledTimes(1);
     expect(mockFs.writeFileSync).toHaveBeenCalledTimes(1);
     expect(mockFs.writeFileSync.mock.calls[0]).toMatchInlineSnapshot(`
       [
-        "root/local-dir/.mtxrc.json",
+        "/root/home-dir/.mtxrc.json",
         "{
         "uaaAppName": "answer 1",
         "regAppName": "answer 2",
@@ -81,10 +86,57 @@ describe("set tests", () => {
       ]
     `);
     expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
-      "using runtime config root/local-dir/.mtxrc.json
-      hit enter to skip a question. re-using the same app for multiple questions is possible.
+      "hit enter to skip a question. re-using the same app for multiple questions is possible.
       wrote runtime config"
     `);
+    expect(mockLogger.error).toHaveBeenCalledTimes(0);
+  });
+
+  test("setup local", async () => {
+    mockContextModule.readRuntimeConfig.mockReturnValueOnce(mockRuntimeConfig);
+    for (let i = 0; i < Object.keys(mockRuntimeConfig).length; i++) {
+      mockStatic.question.mockReturnValueOnce(`answer ${i + 1}`);
+    }
+
+    expect(await set.setupLocal()).toMatchInlineSnapshot(`undefined`);
+    expect(mockStatic.tryAccessSync).toHaveBeenCalledTimes(0);
+    expect(mockContextModule.readRuntimeConfig).toHaveBeenCalledTimes(1);
+    expect(mockFs.writeFileSync).toHaveBeenCalledTimes(1);
+    expect(mockFs.writeFileSync.mock.calls[0]).toMatchInlineSnapshot(`
+      [
+        "/root/local-dir/.mtxrc.json",
+        "{
+        "uaaAppName": "answer 1",
+        "regAppName": "answer 2",
+        "cdsAppName": "answer 3",
+        "hdiAppName": "answer 4",
+        "srvAppName": "answer 5"
+      }
+      ",
+      ]
+    `);
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
+      "hit enter to skip a question. re-using the same app for multiple questions is possible.
+      wrote runtime config"
+    `);
+    expect(mockLogger.error).toHaveBeenCalledTimes(0);
+  });
+
+  test("setup clean cache", async () => {
+    mockStatic.tryAccessSync.mockReturnValueOnce(true);
+    mockStatic.tryAccessSync.mockReturnValueOnce(false);
+
+    set.setupCleanCache();
+    expect(mockStatic.tryAccessSync).toHaveBeenCalledTimes(2);
+    expect(mockFs.unlinkSync).toHaveBeenCalledTimes(1);
+    expect(mockFs.unlinkSync.mock.calls[0]).toMatchInlineSnapshot(`
+      [
+        "root/local-dir/.mtxcache.json",
+      ]
+    `);
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
+      `"removed local cache root/local-dir/.mtxcache.json"`
+    );
     expect(mockLogger.error).toHaveBeenCalledTimes(0);
   });
 });
