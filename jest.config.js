15985aa05613ae5270e9cb3264b4c5846fa6f497
Richard Lindner <rlindner81@gmail.com>
2024-12-06 14:56:54 +0100

    jest config for coverage

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: ["lcov"],
-  coverageReporters: ["text"],
+  coverageReporters: ["lcov"],
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
