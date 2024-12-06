83a685ad3c5fc9726f8288106caa1e6dcc817d21
Richard Lindner <rlindner81@gmail.com>
2024-12-06 16:53:47 +0100

    bring back request exp falloff test

--- test/shared/request.test.js
+++ test/shared/request.test.js
@@ -1,13 +1,19 @@
 "use strict";
-const { request } = require("../../src/shared/request");
-
 const mockFetchLib = require("node-fetch");
 jest.mock("node-fetch", () => jest.fn());
 
 const { Logger: MockLogger } = require("../../src/shared/logger");
-const { outputFromLogger } = require("../test-util/static");
 const mockLogger = MockLogger.getInstance();
 jest.mock("../../src/shared/logger", () => require("../__mocks/shared/logger"));
+
+const mockStatic = require("../../src/shared/static");
+jest.mock("../../src/shared/static", () => ({
+  sleep: jest.fn(),
+}));
+
+const { request } = require("../../src/shared/request");
+
+const { outputFromLogger } = require("../test-util/static");
 
 const baseOkResponse = {
   ok: true,
@@ -26,37 +32,39 @@ const baseBadRequestResponse = {
 
 const outputFromLoggerWithTimestamps = (calls) => outputFromLogger(calls).replace(/\(\d+ms\)/g, "(88ms)");
 
-describe("request", () => {
+describe("request tests", () => {
   test("basic ok", async () => {
     mockFetchLib.mockReturnValueOnce(baseOkResponse);
-    await request({ url: "https://server", pathname: "/path" });
+    await request({ url: "https://fake-server.com", pathname: "/path" });
     expect(mockFetchLib.mock.calls).toMatchSnapshot();
     expect(outputFromLoggerWithTimestamps(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
-      `"GET https://server/path 200 OK (88ms)"`
+      `"GET https://fake-server.com/path 200 OK (88ms)"`
     );
   });
 
   test("basic bad request", async () => {
     mockFetchLib.mockReturnValueOnce(baseBadRequestResponse);
-    await expect(request({ url: "https://server", pathname: "/path" })).rejects.toMatchInlineSnapshot(`
-            [Error: got bad response 400 from https://server/path
+    await expect(request({ url: "https://fake-server.com", pathname: "/path" })).rejects.toMatchInlineSnapshot(`
+            [Error: got bad response 400 from https://fake-server.com/path
             failure reason]
           `);
     expect(mockFetchLib.mock.calls).toMatchSnapshot();
     expect(outputFromLoggerWithTimestamps(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
-      `"GET https://server/path 400 Bad Request (88ms)"`
+      `"GET https://fake-server.com/path 400 Bad Request (88ms)"`
     );
   });
 
   test("bad request unchecked", async () => {
     mockFetchLib.mockReturnValueOnce(baseBadRequestResponse);
-    await expect(request({ url: "https://server", pathname: "/path", checkStatus: false })).resolves.toBeDefined();
+    await expect(
+      request({ url: "https://fake-server.com", pathname: "/path", checkStatus: false })
+    ).resolves.toBeDefined();
     expect(mockFetchLib.mock.calls).toMatchSnapshot();
   });
 
   test("ok with no logging", async () => {
     mockFetchLib.mockReturnValueOnce(baseOkResponse);
-    await request({ url: "https://server", pathname: "/path", logged: false });
+    await request({ url: "https://fake-server.com", pathname: "/path", logged: false });
     expect(mockFetchLib.mock.calls).toMatchSnapshot();
     expect(mockLogger.info).toHaveBeenCalledTimes(0);
   });
@@ -64,7 +72,7 @@ describe("request", () => {
   test("ok with basic auth", async () => {
     mockFetchLib.mockReturnValueOnce(baseOkResponse);
     await request({
-      url: "https://server",
+      url: "https://fake-server.com",
       pathname: "/path",
       auth: { username: "username", password: "test-password" },
     });
@@ -73,7 +81,7 @@ describe("request", () => {
 
   test("ok with bearer auth", async () => {
     mockFetchLib.mockReturnValueOnce(baseOkResponse);
-    await request({ url: "https://server", pathname: "/path", auth: { token: "test-token" } });
+    await request({ url: "https://fake-server.com", pathname: "/path", auth: { token: "test-token" } });
     expect(mockFetchLib.mock.calls).toMatchSnapshot();
   });
 
@@ -89,8 +97,54 @@ describe("request", () => {
       redirect: true,
     });
     expect(mockFetchLib.mock.calls).toMatchSnapshot();
-    expect(outputFromLoggerWithTimestamps(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
-      `"GET https://server/path?hello=world&foo=bar#hashed 200 OK (88ms)"`
-    );
+    expect(outputFromLoggerWithTimestamps(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`"GET https://server/path?hello=world&foo=bar#hashed 200 OK (88ms)"`);
+  });
+
+  test("handling too many requests with falloff", async () => {
+    const busyResponseFactory = (index) =>
+      Promise.resolve({
+        index,
+        status: 429,
+        statusText: "Too Many Requests",
+        text: () => Promise.resolve("too many requests, try again later"),
+      });
+    const responseCount = 5;
+    const fakeResponses = Array.from({ length: responseCount }).map((_, i) => busyResponseFactory(i));
+    for (const fakeResponse of fakeResponses) {
+      mockFetchLib.mockReturnValueOnce(fakeResponse);
+    }
+
+    const responsePromise = request({ url: "https://fake-server.com" });
+
+    await expect(responsePromise).rejects.toMatchInlineSnapshot(`
+            [Error: got bad response 429 from https://fake-server.com/
+            too many requests, try again later]
+          `);
+    expect(mockStatic.sleep.mock.calls).toMatchInlineSnapshot(`
+      [
+        [
+          6000,
+        ],
+        [
+          12000,
+        ],
+        [
+          24000,
+        ],
+        [
+          48000,
+        ],
+      ]
+    `);
+
+    expect(mockFetchLib).toHaveBeenCalledTimes(responseCount);
+    expect(outputFromLoggerWithTimestamps(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
+      "GET https://fake-server.com/ 429 Too Many Requests (88ms) retrying in 6sec
+      GET https://fake-server.com/ 429 Too Many Requests (88ms) retrying in 12sec
+      GET https://fake-server.com/ 429 Too Many Requests (88ms) retrying in 24sec
+      GET https://fake-server.com/ 429 Too Many Requests (88ms) retrying in 48sec
+      GET https://fake-server.com/ 429 Too Many Requests (88ms)"
+    `);
+    expect(mockLogger.error).toHaveBeenCalledTimes(0);
   });
 });
