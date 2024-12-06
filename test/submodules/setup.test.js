c695de1f324cdef66a9e4b451df3841a75046f86
Richard Lindner <rlindner81@gmail.com>
2024-12-06 11:22:18 +0100

    something is still off...

--- test/submodules/setup.test.js
+++ test/submodules/setup.test.js
@@ -123,20 +123,65 @@ describe("set tests", () => {
   });
 
   test("setup clean cache", async () => {
-    mockStatic.tryAccessSync.mockReturnValueOnce(true);
+    mockStatic.tryAccessSync.mockReturnValueOnce(true); // local try is a hit
+    mockStatic.tryAccessSync.mockReturnValueOnce(false);
+    mockStatic.tryAccessSync.mockReturnValueOnce(false);
+    mockStatic.tryAccessSync.mockReturnValueOnce(false);
+    mockStatic.tryAccessSync.mockReturnValueOnce(true); // global try is also a hit
     mockStatic.tryAccessSync.mockReturnValueOnce(false);
 
     set.setupCleanCache();
-    expect(mockStatic.tryAccessSync).toHaveBeenCalledTimes(2);
-    expect(mockFs.unlinkSync).toHaveBeenCalledTimes(1);
-    expect(mockFs.unlinkSync.mock.calls[0]).toMatchInlineSnapshot(`
+    expect(mockStatic.tryAccessSync.mock.calls).toMatchInlineSnapshot(`
       [
-        "root/local-dir/.mtxcache.json",
+        [
+          "/root/local-dir/.mtxcache.json",
+          4,
+        ],
+        [
+          "/root/local-dir/.mtxcache.json",
+          4,
+        ],
+        [
+          "/root/.mtxcache.json",
+          4,
+        ],
+        [
+          "/.mtxcache.json",
+          4,
+        ],
+        [
+          "/root/home-dir/.mtxcache.json",
+          4,
+        ],
+        [
+          "/root/local-dir/.mtxcache.json",
+          4,
+        ],
+        [
+          "/root/.mtxcache.json",
+          4,
+        ],
+        [
+          "/.mtxcache.json",
+          4,
+        ],
+        [
+          "/root/home-dir/.mtxcache.json",
+          4,
+        ],
       ]
     `);
-    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(
-      `"removed local cache root/local-dir/.mtxcache.json"`
-    );
+    // expect(mockStatic.tryAccessSync).toHaveBeenCalledTimes(8);
+    expect(mockFs.unlinkSync).toHaveBeenCalledTimes(2);
+    expect(mockFs.unlinkSync.mock.calls[0]).toMatchInlineSnapshot(`
+      [
+        "/root/local-dir/.mtxcache.json",
+      ]
+    `);
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
+      "removed local cache /root/local-dir/.mtxcache.json
+      removed global cache /root/home-dir/.mtxcache.json"
+    `);
     expect(mockLogger.error).toHaveBeenCalledTimes(0);
   });
 });
