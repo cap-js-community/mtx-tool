ad0705575fc2301d5322481f6566eeb9ffca7946
Richard Lindner <rlindner81@gmail.com>
2025-06-11 15:22:20 +0200

    coverage can include server diagnostics

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.js"],
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ["/node_modules/", "/bin/", "src/submodules/serverDiagnostic.js"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/bin/"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
