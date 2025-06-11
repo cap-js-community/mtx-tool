9e60140cd0af7415a19bfc18549842c471be3b37
Richard Lindner <rlindner81@gmail.com>
2025-06-11 10:09:37 +0200

    srv debug test 2

--- test/submodules/serverDiagnostic.test.js
+++ test/submodules/serverDiagnostic.test.js
@@ -6,6 +6,7 @@ jest.mock("../../src/shared/logger", () => require("../__mocks/shared/logger"));
 
 const srv = require("../../src/submodules/serverDiagnostic");
 const mockAppName = "mock-app-name";
+const mockAppNameAlt = "mock-app-name-alt";
 
 jest.mock("../../src/shared/static", () => require("../__mocks/sharedNockPlayback/static"));
 
@@ -22,16 +23,24 @@ const mockContext = {
     };
   },
   getAppNameInfoCached(appName) {
-    if (appName !== mockAppName) {
-      throw new Error("unexpected appName");
+    switch (appName) {
+      case mockAppName:
+        return { cfSsh: mockCfSsh, cfBuildpack: "nodejs_buildpack", cfAppGuid: "mock-app-guid" };
+      case mockAppNameAlt:
+        return {
+          cfSsh: mockCfSsh,
+          cfBuildpack: "https://github.com/cloudfoundry/nodejs-buildpack.git#v1.8.33",
+          cfAppGuid: "mock-app-alt-guid",
+        };
+      default:
+        throw new Error("unexpected appName");
     }
-    return { cfSsh: mockCfSsh, cfBuildpack: "nodejs_buildpack", cfAppGuid: "mock-app-guid" };
   },
 };
 
 describe("srv tests", () => {
   test("srv debug default", async () => {
-    await expect(srv.serverDebug(mockContext, [])).resolves.toMatchInlineSnapshot(`undefined`);
+    await expect(srv.serverDebug(mockContext, [])).resolves.toBeUndefined();
     expect(mockCfSsh).toHaveBeenCalledTimes(1);
     expect(mockCfSsh.mock.calls[0]).toMatchInlineSnapshot(`
       [
@@ -51,8 +60,37 @@ describe("srv tests", () => {
     expect(mockLogger.error).toHaveBeenCalledTimes(0);
   });
 
-  test("srv debug custom", async () => {
-    await expect(srv.serverDebug(mockContext, [mockAppName, "1"])).resolves.toMatchInlineSnapshot(`undefined`);
+  test("srv debug custom mockApp instance 0", async () => {
+    await expect(srv.serverDebug(mockContext, [mockAppName, "0"])).resolves.toBeUndefined();
+    expect(mockCfSsh).toHaveBeenCalledTimes(2);
+    expect(mockCfSsh.mock.calls[0]).toMatchInlineSnapshot(`
+      [
+        {
+          "appInstance": "0",
+          "command": "pkill --signal SIGUSR1 node",
+        },
+      ]
+    `);
+    expect(mockCfSsh.mock.calls[1]).toMatchInlineSnapshot(`
+      [
+        {
+          "appInstance": "0",
+          "localPort": 9229,
+          "remotePort": 9229,
+        },
+      ]
+    `);
+    expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
+      "
+      connect node debugger on port 9229
+      use request header "X-Cf-App-Instance: mock-app-guid:0" to target this app instance
+      "
+    `);
+    expect(mockLogger.error).toHaveBeenCalledTimes(0);
+  });
+
+  test("srv debug custom mockAppAlt instance 1", async () => {
+    await expect(srv.serverDebug(mockContext, [mockAppNameAlt, "1"])).resolves.toBeUndefined();
     expect(mockCfSsh).toHaveBeenCalledTimes(2);
     expect(mockCfSsh.mock.calls[0]).toMatchInlineSnapshot(`
       [
@@ -74,7 +112,7 @@ describe("srv tests", () => {
     expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
       "
       connect node debugger on port 9229
-      use request header "X-Cf-App-Instance: mock-app-guid:1" to target this app instance
+      use request header "X-Cf-App-Instance: mock-app-alt-guid:1" to target this app instance
       "
     `);
     expect(mockLogger.error).toHaveBeenCalledTimes(0);
