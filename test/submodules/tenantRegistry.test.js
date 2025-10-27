1bed75e805d3b645572b3b9cd1e69ef9ba052bf2
Richard Lindner <rlindner81@gmail.com>
2025-10-27 11:06:01 +0100

    fix resnap

--- test/submodules/tenantRegistry.test.js
+++ test/submodules/tenantRegistry.test.js
@@ -242,48 +242,48 @@ describe("reg tests", () => {
 
       await expect(reg.registryUpdateAllDependencies(fakeContextOnlyReg, undefined, [])).resolves
         .toMatchInlineSnapshot(`
-            [
-              {
-                "duration": "0 sec",
-                "jobId": "11111111-0000-0000-0000-000000000001",
-                "jobState": "SUCCEEDED",
-                "tenantId": "00000000-0000-0000-0000-000000000001",
-                Symbol(IS_SUCCESS): true,
-              },
-              {
-                "duration": "0 sec",
-                "jobId": "11111111-0000-0000-0000-000000000002",
-                "jobState": "SUCCEEDED",
-                "tenantId": "00000000-0000-0000-0000-000000000002",
-                Symbol(IS_SUCCESS): true,
-              },
-              {
-                "duration": "0 sec",
-                "jobId": "11111111-0000-0000-0000-000000000003",
-                "jobState": "SUCCEEDED",
-                "tenantId": "00000000-0000-0000-0000-000000000003",
-                Symbol(IS_SUCCESS): true,
-              },
-              {
-                "duration": "0 sec",
-                "jobId": "11111111-0000-0000-0000-000000000004",
-                "jobState": "SUCCEEDED",
-                "tenantId": "00000000-0000-0000-0000-000000000004",
-                Symbol(IS_SUCCESS): true,
-              },
-            ]
-          `);
+              [
+                {
+                  "duration": "0 sec",
+                  "jobId": "11111111-0000-0000-0000-000000000001",
+                  "jobState": "SUCCEEDED",
+                  "tenantId": "00000000-0000-0000-0000-000000000001",
+                  Symbol(IS_SUCCESS): true,
+                },
+                {
+                  "duration": "0 sec",
+                  "jobId": "11111111-0000-0000-0000-000000000002",
+                  "jobState": "SUCCEEDED",
+                  "tenantId": "00000000-0000-0000-0000-000000000002",
+                  Symbol(IS_SUCCESS): true,
+                },
+                {
+                  "duration": "0 sec",
+                  "jobId": "11111111-0000-0000-0000-000000000003",
+                  "jobState": "SUCCEEDED",
+                  "tenantId": "00000000-0000-0000-0000-000000000003",
+                  Symbol(IS_SUCCESS): true,
+                },
+                {
+                  "duration": "0 sec",
+                  "jobId": "11111111-0000-0000-0000-000000000004",
+                  "jobState": "SUCCEEDED",
+                  "tenantId": "00000000-0000-0000-0000-000000000004",
+                  Symbol(IS_SUCCESS): true,
+                },
+              ]
+            `);
 
       expect(mockRequest.request).toHaveBeenCalledTimes(1 + n * 2);
 
       expect(mockShared.sleep.mock.calls).toMatchSnapshot();
 
       expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
-      "polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000001 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000002 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000003 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000004 with interval 15sec"
-    `);
+        "polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000001 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000002 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000003 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000004 with interval 15sec"
+      `);
       expect(mockLogger.error).toHaveBeenCalledTimes(0);
     });
 
@@ -315,11 +315,11 @@ describe("reg tests", () => {
       expect(mockShared.sleep.mock.calls).toMatchSnapshot();
 
       expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
-      "polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000001 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000002 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000003 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000004 with interval 15sec"
-    `);
+        "polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000001 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000002 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000003 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000004 with interval 15sec"
+      `);
       expect(outputFromLogger(mockLogger.error.mock.calls)).toMatchInlineSnapshot(`
         "[
           {
@@ -381,38 +381,38 @@ describe("reg tests", () => {
       expect(mockShared.sleep.mock.calls).toMatchSnapshot();
 
       expect(outputFromLogger(mockLogger.error.mock.calls)).toMatchInlineSnapshot(`
-      "[
-        {
-          "tenantId": "00000000-0000-0000-0000-000000000001",
-          "duration": "0 sec",
-          "jobId": "11111111-0000-0000-0000-000000000001",
-          "jobState": "SUCCEEDED"
-        },
-        {
-          "tenantId": "00000000-0000-0000-0000-000000000002",
-          "duration": "0 sec",
-          "jobId": "11111111-0000-0000-0000-000000000002",
-          "jobState": "SUCCEEDED"
-        },
-        {
-          "tenantId": "00000000-0000-0000-0000-000000000003",
-          "duration": "0 sec",
-          "error": "server feels ill"
-        },
-        {
-          "tenantId": "00000000-0000-0000-0000-000000000004",
-          "duration": "0 sec",
-          "jobId": "11111111-0000-0000-0000-000000000004",
-          "jobState": "SUCCEEDED"
-        }
-      ]"
-    `);
+        "[
+          {
+            "tenantId": "00000000-0000-0000-0000-000000000001",
+            "duration": "0 sec",
+            "jobId": "11111111-0000-0000-0000-000000000001",
+            "jobState": "SUCCEEDED"
+          },
+          {
+            "tenantId": "00000000-0000-0000-0000-000000000002",
+            "duration": "0 sec",
+            "jobId": "11111111-0000-0000-0000-000000000002",
+            "jobState": "SUCCEEDED"
+          },
+          {
+            "tenantId": "00000000-0000-0000-0000-000000000003",
+            "duration": "0 sec",
+            "error": "server feels ill"
+          },
+          {
+            "tenantId": "00000000-0000-0000-0000-000000000004",
+            "duration": "0 sec",
+            "jobId": "11111111-0000-0000-0000-000000000004",
+            "jobState": "SUCCEEDED"
+          }
+        ]"
+      `);
       expect(outputFromLogger(mockLogger.info.mock.calls)).toMatchInlineSnapshot(`
-      "polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000001 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000002 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000003 with interval 15sec
-      polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000004 with interval 15sec"
-    `);
+        "polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000001 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000002 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000003 with interval 15sec
+        polling subscription /api/v2.0/jobs/11111111-0000-0000-0000-000000000004 with interval 15sec"
+      `);
     });
   });
   describe("mixed reg and sms", () => {
