cc872fa1de0caf8ff5b78731d3bce7b33a964389
Richard Lindner <rlindner81@gmail.com>
2025-11-13 11:11:30 +0100

    remove getCachedUaaToken API 3

--- test/context.test.js
+++ test/context.test.js
@@ -52,8 +52,7 @@ describe("context tests", () => {
     mockRequest.mockReturnValueOnce({ json: () => mockCfProcess });
     mockRequest.mockReturnValueOnce({ json: () => mockCfRoutes });
 
-    // TODO remove API
-    await expect(context.getCachedUaaToken()).rejects.toMatchInlineSnapshot(
+    await expect(context.getUaaInfo()).rejects.toMatchInlineSnapshot(
       `[Error: no vcap service information in environment, check cf user permissions]`
     );
   });
